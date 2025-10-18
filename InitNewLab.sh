371fc153fce949f23bc19d237c3e1a6b8e074496
d-asv-b <dlasvlb@gmail.com>
2025-10-18 15:18:02 +0000

    Add Lab3. Fixes in Lab1 and Lab2

--- InitNewLab.sh
+++ InitNewLab.sh
@@ -5,7 +5,7 @@ read repo_name
 
 mkdir $repo_name
 
-mkdir $repo_name/include $repo_name/src $repo_name/tests
+mkdir $repo_name/include $repo_name/src $repo_name/test
 
 git init $repo_name
-touch $repo_name/CMakeLists.txt $repo_name/main.cpp $repo_name/tests/test.cpp
+touch $repo_name/CMakeLists.txt $repo_name/main.cpp $repo_name/test/test.cpp
