fab95defea91097c2d68121034cec4edc6899b49
d-asv-b <dlasvlb@gmail.com>
2025-11-07 21:48:42 +0000

    Some changes and added Lab4 repository as submodule

--- InitNewLab.sh
+++ InitNewLab.sh
@@ -9,3 +9,5 @@ mkdir $repo_name/include $repo_name/src $repo_name/test
 
 git init $repo_name
 touch $repo_name/CMakeLists.txt $repo_name/main.cpp $repo_name/test/test.cpp
+
+cp ./.gitignore $repo_name/.gitignore
